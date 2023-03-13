16b54313fcd18a1c414406f96de5f2ae8dea047e
Svein Jakob Høie <sveinjakob@hoie.dev>
2023-03-13 21:42:43 +0100

    Initial commit

---
+++ app/kontakt/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+
+export default function Contact() {
+    return (
+        <div className={styles.contact}>
+            <h3>Kontakt</h3>
+            <p>Svein Jakob Høie</p>
+            <p>sveinjakobhoie@gmail.com</p>
+            <p>941 41 907</p>
+            <p>Buhagen 2, 4322 Sandnes</p>
+        </div>
+    );
+}
